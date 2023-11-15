65bcf00e97203fa432674b388e3110dcc63ca5ce
AXBIT\ssumenkov <ssumenkov@axbit.ru>
2023-11-15 09:50:56 +0400

    +

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from "react";
-import '../scss/style.css';
-
+import "../scss/style.css";
 
 const Time: React.FC = () => {
-    
-    const [time, setTime] = useState<string | undefined>();
-    useEffect(() => {
-        setInterval(() => {
-            const date = new Date();
-            setTime(date.toLocaleTimeString());
-        }, 1000);
-    }, []);
-    
-    return (
-        <div className="time">
-          <div className="time__card">
-            <p className="time__descr">{time}</p>
-          </div>
-        </div>
-    )
-}
+  const [time, setTime] = useState<string | undefined>();
+  const intervalRef = React.useRef<number | undefined>();
 
+  useEffect(() => {
+    intervalRef.current = window.setInterval(() => {
+      const date = new Date();
+      setTime(date.toLocaleString());
+    }, 1000);
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="time">
+      <div className="time__card">
+        <p className="time__descr">{time}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Time;
